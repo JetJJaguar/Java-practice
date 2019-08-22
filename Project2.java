9c11d47a7f2f8dd586d2f9b757383ec1ef16b044
JetJJaguar <38378704+JetJJaguar@users.noreply.github.com>
2019-08-22 15:50:50 -0700

    Add files via upload

---
+++ Project2.java
@@ -0,0 +1,32 @@
+	public class Project2 {
+ public static void main(String[] args){
+	for (int i = 1; i <= 5; i++) {
+	 DrawHeart();}
+	for (int i = 1; i <= 10; i++) {
+	DrawFace();
+	}
+ } 
+	 public static void DrawHeart() {
+			System.out.print("\n       .--.  .--.\n");
+			System.out.println("      :  _ \\/ _  :");
+			System.out.println("   _\\/ \\ 6    6 /");
+			System.out.println("     \\__\\  '   /");
+			System.out.println("         \\'--'/ \\__/_ ");
+			System.out.println("         /\\  /\\    \\ ");
+			System.out.println("        /  \\/  \\");
+			System.out.println("        \\      /");
+			System.out.println("   jgs  _\\    /_");
+			System.out.println("       (__\\  /__)\n");
+ }
+	 public static void DrawFace() {
+		 System.out.println("               .-\"\"\"\"\"\"-.  ");
+	    	System.out.println("             .'          '. ");
+			System.out.println("            /   O      O   \\ ");
+			System.out.println("           :                :");
+			System.out.println("           |                |  goofy");
+			System.out.println("           : ',          ,' :");
+			System.out.println("            \\  '-.____.-'  / ");
+			System.out.println("             '.     U    .'");
+			System.out.println("          jgs  '-......-' \n");
+	 }
+}
